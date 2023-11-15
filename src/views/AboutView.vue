ff653f4eaebc74032f24ab365d74352ced5c139d
xinminsu <sinosu@163.com>
2023-11-15 16:55:21 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <a-card hoverable style="width: 240px">
+    <template #cover>
+      <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+    </template>
+    <a-card-meta title="Europe Street beat">
+      <template #description>www.instagram.com</template>
+    </a-card-meta>
+  </a-card>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-row>
+    <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 6, offset: 2 }">
+      <a-card hoverable style="width: 240px">
+        <template #cover>
+          <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+        </template>
+        <a-card-meta title="Europe Street beat">
+          <template #description>www.instagram.com</template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+    <a-col :xs="{ span: 11, offset: 1 }" :lg="{ span: 6, offset: 2 }">
+      <a-card hoverable style="width: 240px">
+        <template #cover>
+          <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+        </template>
+        <a-card-meta title="Europe Street beat">
+          <template #description>www.instagram.com</template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+    <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 6, offset: 2 }">
+      <a-card hoverable style="width: 240px">
+        <template #cover>
+          <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+        </template>
+        <a-card-meta title="Europe Street beat">
+          <template #description>www.instagram.com</template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+  </a-row>
+</template>
